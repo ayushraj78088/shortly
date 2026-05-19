00b5e5d85c48549b1f822bbcda0aa4181059a166
ayushraj78088 <ayushraj78088@gmail.com>
2026-05-20 00:02:30 +0530

    Create routes for url

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/utils/db.js";
+import cors from "cors";
+import urlRoutes from "./src/routes/url.js";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, everything is working fine");
-});
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  }),
+);
+
+app.use(express.json());
+
+app.use("/", urlRoutes);
 
 app.listen(3000, () => {
   console.log("Server is working correctly");
